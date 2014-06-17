f865ee0349f8e69dc3a974e31a6ceb0a22c5ff60
Robert Thompson <rthomps7@nd.edu>
2014-06-16 20:53:48 -0400

    Adding response schema detector. Will be good for documentation

--- test/basic.js
+++ test/basic.js
@@ -3,20 +3,28 @@ var crud = require('../index.js')(),
     cbax = require('cbax'),
     utools = require('utools'),
     express = require('express'),
+    debug = require('debug')('api'),
     app = express();
 
 crud.entity('/users')
     .c(crud.cb({
       username: { type: 'string', description: 'this is my description' }
     }, create_user))
-    .r(create_user)
+    .r(crud.cb(
+      { username: { type: 'string', description: 'this is my description' } },
+      create_user,
+      { username: { type: 'string', description: 'this is my description' } }
+    ))
     .u(create_user)
     .d(create_user);
 
 crud.launch(app);
 
+app.listen('3000', function() {
+});
+
+
 function create_user(d, cb, req, res) {
-  console.log('datum', d);
-  cb(null, true);
+  cb(null, { username: 'bobby' });
 }
 
