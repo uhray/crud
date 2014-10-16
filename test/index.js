6c3d943c95bd8d0e57890ada332265613837ef1f
Robert Thompson <rthomps7@nd.edu>
2014-10-16 14:10:09 -0400

    events and debugs

--- test/index.js
+++ test/index.js
@@ -9,11 +9,23 @@ app.use(require('body-parser').json());
 crud.entity('/users').Read()
   .use(function(req, res, next) {
     next();
-  })
-  .pipe(function(data, query, cb) {
-    console.log(this);
+  }).pipe(function(data, query, cb) {
+    cb(null, [ { name: 'bobby tables' } ]);
+  }).pipe(function(data, query, cb) {
     cb();
+  }).on('close', function(d) {
+    console.log('CLOSED READ', d);
+  }).on('error', function(d) {
+    console.log('ERROR READ', d);
   });
+
+crud.entity('/users')
+    .on('open', function(method, data, query) {
+      console.log('ENTITY OPENED: %s %j %j', method, data, query)
+    })
+    .on('close', function(method, data) {
+      console.log('ENTITY CLOSED: %s %j', method, data)
+    });
 
 crud.launch(app);
 
